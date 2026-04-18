815ed9d017549ca374d8f37bafa0b10557e21cba
hunter-hongg <2843249813@qq.com>
2026-04-18 11:45:25 +0800

    feat: 完成cat -b

--- src/commands/cat.c
+++ src/commands/cat.c
@@ -6,12 +6,14 @@
 
 void cat_command(gint argc, gchar** argv) {
     int show_line_numbers = 0;
+    int show_nonempty_line_numbers = 0;
     
     struct arg_lit* number_opt = arg_lit0("n", "number", "number all output lines");
+    struct arg_lit* nonempty_number_opt = arg_lit0("b", "number-nonblank", "number nonempty output lines");
     struct arg_file* file_arg = arg_filen(NULL, NULL, "FILE", 0, 100, "file to read");
     struct arg_end* end = arg_end(20);
 
-    void* argtable[] = { number_opt, file_arg, end };
+    void* argtable[] = { number_opt, nonempty_number_opt, file_arg, end };
 
     int nerrors = arg_parse(argc, argv, argtable);
 
@@ -22,6 +24,12 @@ void cat_command(gint argc, gchar** argv) {
     }
 
     show_line_numbers = (number_opt->count > 0);
+    show_nonempty_line_numbers = (nonempty_number_opt->count > 0);
+    
+    // According to POSIX, -b overrides -n
+    if (show_nonempty_line_numbers) {
+        show_line_numbers = 0;
+    }
 
     int line_num = 1;
     FILE* fp;
@@ -31,6 +39,12 @@ void cat_command(gint argc, gchar** argv) {
         while (fgets(buf, sizeof(buf), fp)) {
             if (show_line_numbers) {
                 printf("%6d  %s", line_num++, buf);
+            } else if (show_nonempty_line_numbers) {
+                if (buf[0] != '\n') {
+                    printf("%6d  %s", line_num++, buf);
+                } else {
+                    printf("%s", buf);
+                }
             } else {
                 printf("%s", buf);
             }
@@ -50,6 +64,12 @@ void cat_command(gint argc, gchar** argv) {
             while (fgets(buf, sizeof(buf), fp)) {
                 if (show_line_numbers) {
                     printf("%6d  %s", line_num++, buf);
+                } else if (show_nonempty_line_numbers) {
+                    if (buf[0] != '\n') {
+                        printf("%6d  %s", line_num++, buf);
+                    } else {
+                        printf("%s", buf);
+                    }
                 } else {
                     printf("%s", buf);
                 }
