fc2a8692f4647464f6183d96ab3a8135fa3264ba
hunter-hongg <2843249813@qq.com>
2026-06-19 11:12:42 +0800

    feat: add fd command header

---
+++ include/commands/fd.h
@@ -0,0 +1,37 @@
+#ifndef FD_H
+#define FD_H
+
+#include <glib.h>
+
+typedef enum {
+    FD_COLOR_NEVER,
+    FD_COLOR_ALWAYS,
+    FD_COLOR_AUTO
+} fd_color_t;
+
+typedef struct {
+    int hidden;
+    int no_ignore;
+    int case_sensitive;
+    int ignore_case;
+    int smart_case;
+    int glob_mode;
+    int full_path;
+    int follow;
+    int print0;
+    int max_depth;
+    int max_results;
+    fd_color_t color_mode;
+    char type_filter;
+    gchar *pattern;
+    GPtrArray *extensions;
+    GPtrArray *exclude;
+    int has_exec;
+    int exec_batch;
+    GPtrArray *exec_args;
+    GPtrArray *exec_paths;
+} FdOptions;
+
+void fd_command(gint argc, gchar **argv);
+
+#endif
